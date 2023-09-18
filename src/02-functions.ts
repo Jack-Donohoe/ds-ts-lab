445e3d6a0a2853ed5651cffe178b754c478a9569
Jack Donohoe <20093738@mail.wit.ie>
2023-09-18 12:54:56 +0100

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -70,3 +70,14 @@ function findFriends(friends: Friend[],
 
 console.log(findFriends(friends, (friend) => friend.name.startsWith('Co')));
 console.log(findFriends(friends, (friend) => friend.age < 35));
+
+function addInterest(friend:Friend, interest: string){
+    if (friend.interests != undefined){
+        friend.interests.push(interest)
+    } else {
+        friend.interests = [interest]
+    }
+    return friend.interests
+}
+
+console.log(addInterest(friends[1], 'Politics'))
